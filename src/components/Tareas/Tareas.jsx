7bbbe18c2ef86008bbe2b7dc2714999a81238819
Edwin Rozo - edwindev <erozog125@gmail.com>
2024-09-21 14:25:37 -0500

    Aprendiendo hooks

---
+++ src/components/Tareas/Tareas.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './tareas.css'
+
+export const Tareas = () => {
+  return (
+    <div>Tareas</div>
+  )
+}
---
+++ src/components/UseState/UseState.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const UseState = () => {
+  return (
+    <>
+      <h1>Yo soy el contador</h1>
+      <button>Sumar</button>    
+    </>
+  )
+}
